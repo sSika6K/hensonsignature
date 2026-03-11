1f140d949e59499d6f673cb6cdef8d98e21804c2
Armand Brethès <armand.brethes@gmail.com>
2026-03-11 16:01:43 +0100

    fix: next/react moved to dependencies + ReactNode type

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-c042ca735a68d6ec.js","static/chunks/4bd1b696-c023c6e3521b1417.js","static/chunks/255-2fe823c2b1ce4f82.js","static/chunks/main-app-6fc4947c24f35841.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-c042ca735a68d6ec.js","static/chunks/framework-de98b93a850cfc71.js","static/chunks/main-4fa758ed5eca4ac5.js","static/chunks/pages/_app-82835f42865034fa.js"],"/_error":["static/chunks/webpack-c042ca735a68d6ec.js","static/chunks/framework-de98b93a850cfc71.js","static/chunks/main-4fa758ed5eca4ac5.js","static/chunks/pages/_error-013f4188946cdd04.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/20d1a56625def336.js",
+    "static/chunks/806bdb8e4a6a9b95.js",
+    "static/chunks/aee6c7720838f8a2.js",
+    "static/chunks/d702a24e2b6c48fa.js",
+    "static/chunks/turbopack-835a025b93150d23.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BJKo/oDlBfUHaH5BWztD8xaIoF1wJi0FLfVUx17xiI8=\"\n}"
